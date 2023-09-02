7e752c005ce64b3f6db0529e9b20ac29f7042f79
Jack Clarke <jackfulcher09@gmail.com>
2023-09-02 14:48:54 -0400

    Update config

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one-light)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -41,6 +41,11 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+
+(after! org
+  (add-to-list 'org-capture-templates
+                '("s" "Scientific Method" entry (file+headline "sciences.org" "Questions")
+                   "* Question:\n** Hypotheses\n*** <Your Hypothesis>\n**** Test Procedure\n***** Results\n**** Verified?")))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
