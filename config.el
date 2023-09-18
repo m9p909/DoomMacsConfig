b0332f5eaf80fa61d2abfdf99e6c89cc03777f74
Jack Clarke <jack.clarke@solace.com>
2023-09-18 16:31:48 -0400

    update config

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-solarized-dark-high-contrast)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -46,6 +46,13 @@
   (add-to-list 'org-capture-templates
                 '("s" "Scientific Method" entry (file+headline "sciences.org" "Questions")
                    "* Question:\n** Hypotheses\n*** <Your Hypothesis>\n**** Test Procedure\n***** Results\n**** Verified?")))
+
+(map! :after python
+      :localleader
+      (:map python-mode-map
+            "ee" #'python-shell-send-region
+            "eb" #'python-shell-send-buffer
+            ))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
